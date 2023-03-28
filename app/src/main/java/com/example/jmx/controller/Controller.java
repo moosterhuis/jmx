1b96231304e43aee67bdc55fc74f9e5bb1ca2fca
moosterhuis <116188916+moosterhuis@users.noreply.github.com>
2023-03-28 16:09:01 +0200

    initial commit

---
+++ app/src/main/java/com/example/jmx/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.example.jmx.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Controller {
+
+    @GetMapping
+    String hello() {
+        return "hello";
+    }
+}
